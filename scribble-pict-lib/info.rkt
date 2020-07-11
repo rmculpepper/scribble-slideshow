6278febef4450dc0ea331ac4050aeb59af140c46
Ryan Culpepper <ryanc@racket-lang.org>
2020-07-12 01:50:02 +0200

    info.rkt

---
+++ scribble-pict-lib/info.rkt
@@ -0,0 +1,12 @@
+#lang info
+
+;; pkg info
+
+(define version "0.1")
+(define collection "scribble-pict")
+(define deps '("pict-lib" "scribble-lib"))
+(define pkg-authors '(ryanc))
+
+;; collection info
+
+(define name "scribble-pict")
