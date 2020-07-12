bf4c1f814ddaff4a45b7a6498fd7dd7c1ece3ebc
Ryan Culpepper <ryanc@racket-lang.org>
2020-07-12 06:18:06 +0200

    change language to export module-begin from submodule

--- scribble-slideshow-lib/main.rkt
+++ scribble-slideshow-lib/main.rkt
@@ -1,30 +1,34 @@
 #lang racket/base
-(require (only-in scribble/doclang [#%module-begin scribble-module-begin])
-         "pict.rkt"
+(require "pict.rkt"
          "private/slide.rkt")
-(provide (except-out (all-from-out racket/base) #%module-begin)
-         (rename-out [module-begin #%module-begin])
-         (all-from-out "pict.rkt")
+(provide (all-from-out "pict.rkt")
          (all-from-out "private/slide.rkt"))
 
 ;; ----------------------------------------
-;; Module wrapper
+;; Language
 
-(define-syntax-rule (module-begin id . body)
-  (scribble-module-begin
-   id post-process ()
-   (module* main racket/base
-     (require scribble-slideshow
-              (only-in (submod "..") [id id]))
-     (scribble-slides* id))
-   . body))
+(module* lang racket/base
+  (require (only-in scribble/doclang [#%module-begin scribble-module-begin])
+           (submod ".."))
+  (provide (except-out (all-from-out racket/base) #%module-begin)
+           (rename-out [module-begin #%module-begin])
+           (all-from-out (submod "..")))
 
-(define (post-process doc) doc)
+  (define-syntax-rule (module-begin id . body)
+    (scribble-module-begin
+     id post-process ()
+     (module* main racket/base ;; module+ does not seem to work ...
+       (require scribble-slideshow
+                (only-in (submod "..") [id id]))
+       (scribble-slides* id))
+     . body))
+
+  (define (post-process doc) doc))
 
 ;; ----------------------------------------
 ;; Reader
 
 ;; Adapted from scribble/manual/lang/reader.rkt
 (module reader scribble/base/reader
-  scribble-slideshow
+  (submod scribble-slideshow lang)
   #:wrapper1 (lambda (t) (cons 'doc (t))))
