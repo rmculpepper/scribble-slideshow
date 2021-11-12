d3f4b0ff49073b51ad491678d24f7a2da4f6d7b8
Ryan Culpepper <ryanc@racket-lang.org>
2021-11-12 14:59:03 +0100

    ... content

--- scribble-slideshow-lib/private/content.rkt
+++ scribble-slideshow-lib/private/content.rkt
@@ -65,6 +65,27 @@
      (define ptstyle (append (hash-ref istyle 'text-mods null) (hash-ref istyle 'text-base)))
      (finish (text str ptstyle (hash-ref istyle 'text-size)) istyle #t)]))
 
+;; A Segment is a String that contains either
+;; - no whitespace characters
+;; - one or more linear whitespace characters (space and tab)
+;; - exactly one newline ("\n"); may be multiple chars in source, eg "\n\r"
+(define (string->segments s wsmode)
+  (define ws-rx (if wsmode #rx"[ \t]+|\n\r?|\r" #px"\\s+"))
+  (define (conv-ws s)
+    (case wsmode
+      [(pre pre-wrap) (case s [("\n" "\r" "\n\r") "\n"] [else s])]
+      [(nowrap) (case s [("\n" "\r" "\n\r") " "] [else s])]
+      [else #;(#f)  " "]))
+  (define ws-zones (regexp-match-positions* #px"[ \t]+|\n\r?|\r" s))
+  (let loop ([start 0] [ws-zones ws-zones])
+    (cond [(null? ws-zones)
+           (if (< start (string-length s)) (list (substring s start)) null)]
+          [(< start (caar ws-zones))
+           (cons (substring s start (caar ws-zones)) (loop (caar ws-zones) ws-zones))]
+          [else
+           (cons (conv-ws (substring s (caar ws-zones) (cdar ws-zones)))
+                 (loop (cdar ws-zones) (cdr ws-zones)))])))
+
 ;; ------------------------------------------------------------
 
 (define (content->pict/v1 content istyle width)
@@ -85,15 +106,16 @@
 ;;
 ;; 3. Pack lines using unbreakable sequences.
 
-;; A Fragment is (fragment Pict WSMode), where a pict originating
-;; from a string either contains no whitespace or only whitespace.
+;; A Fragment is one of
+;; - (fragment Pict WSMode), where a pict originating
+;;   from a string either contains no whitespace or only whitespace,
+;; - 'nl
 (struct fragment (pict ws) #:prefab)
 (struct fragment:string fragment (str istyle) #:prefab)
 
 ;; A WSMode is one of
 ;; - 'ws    -- soft whitespace: can break line, dropped at EOL
-;; - 'nl    -- hard newline
-;; - #f     -- not whitespace
+;; - #f     -- not whitespace: cannot break, cannot drop
 
 ;; FIXME: handle @|?-|, soft hyphen
 ;; FIXME: handle @nonbreaking{..}, 'no-break style
@@ -106,7 +128,7 @@
        (define wsmode (hash-ref istyle 'white-space #f))
        (for/fold ([acc acc]) ([seg (in-list (string->segments content wsmode))])
          (cond [(equal? seg "\n")
-                (cons (fragment (blank) 'nl) acc)]
+                (cons 'nl acc)]
                [(regexp-match? #rx"^[ \t]+$" seg)
                 (define seg-pict (base-content->pict seg istyle))
                 (case wsmode
@@ -125,8 +147,7 @@
                 (cons (fragment (base-content->pict seg istyle) #f) acc)]))]
       [(? symbol? s) (loop (content-symbol->string s) acc istyle)]
       [(? pict? p) (cons (fragment (base-content->pict p istyle) #f) acc)]
-      [(s:element 'newline '("\n"))
-       (cons (fragment (blank) 'nl) acc)]
+      [(s:element 'newline '("\n")) (cons 'nl acc)]
       [(s:element style content)
        (loop content acc (add-style style istyle))]
       ;; multiarg-element -- ??
@@ -156,6 +177,8 @@
     (match fs
       [(cons (and f (fragment p ws)) fs)
        (inner-loop ws fs (list p) outer-acc)]
+      [(cons 'nl fs)
+       (outer-loop fs (cons 'nl outer-acc))]
       ['() outer-acc]))
   (define (inner-loop ws fs inner-acc outer-acc)
     (match fs
@@ -169,7 +192,7 @@
   (define (outer-loop fs outer-acc)
     (match fs
       ['() (reverse outer-acc)]
-      [(cons (fragment _ 'ws) fs)
+      [(cons (fragment _ 'ws) fs) ;; drop ws at start of line
        (outer-loop fs outer-acc)]
       [fs ;; starts with non-ws fragment
        (inner-loop fs null 0 #f outer-acc)]))
@@ -178,7 +201,7 @@
       (let ([acc (if wsw (cdr acc) acc)])
         (apply hbl-append 0 (reverse acc))))
     (match fs0
-      [(cons (fragment p 'nl) fs)
+      [(cons 'nl fs)
        (outer-loop fs (cons (line) outer-acc))]
       [(cons (fragment p ws?) fs)
        (define pw (pict-width p))
@@ -190,26 +213,5 @@
       ['()
        (outer-loop null (cons (line) outer-acc))]))
   (outer-loop fs null))
-
-;; A Segment is a String that contains either
-;; - no whitespace characters
-;; - one or more linear whitespace characters (space and tab)
-;; - exactly one newline ("\n"); may be multiple chars in source, eg "\n\r"
-(define (string->segments s wsmode)
-  (define ws-rx (if wsmode #rx"[ \t]+|\n\r?|\r" #px"\\s+"))
-  (define (conv-ws s)
-    (case wsmode
-      [(pre pre-wrap) (case s [("\n" "\r" "\n\r") "\n"] [else s])]
-      [(nowrap) (case s [("\n" "\r" "\n\r") " "] [else s])]
-      [else #;(#f)  " "]))
-  (define ws-zones (regexp-match-positions* #px"[ \t]+|\n\r?|\r" s))
-  (let loop ([start 0] [ws-zones ws-zones])
-    (cond [(null? ws-zones)
-           (if (< start (string-length s)) (list (substring s start)) null)]
-          [(< start (caar ws-zones))
-           (cons (substring s start (caar ws-zones)) (loop (caar ws-zones) ws-zones))]
-          [else
-           (cons (conv-ws (substring s (caar ws-zones) (cdar ws-zones)))
-                 (loop (cdar ws-zones) (cdr ws-zones)))])))
 
 ;; ------------------------------------------------------------
