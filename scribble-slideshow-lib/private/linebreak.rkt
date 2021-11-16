bb40f94dfc73f9d26c6522c13de2e16167c264ef
Ryan Culpepper <ryanc@racket-lang.org>
2021-11-16 21:13:01 +0100

    ... WIP linebreak

--- scribble-slideshow-lib/private/linebreak.rkt
+++ scribble-slideshow-lib/private/linebreak.rkt
@@ -1,4 +1,8 @@
 #lang racket/base
+(require racket/match
+         (only-in racket/math sqr)
+         racket/list
+         racket/class)
 (provide (all-defined-out))
 
 ;; References:
@@ -27,13 +31,184 @@
 
 ;; ------------------------------------------------------------
 
+(define ALPHA 1000)
+(define GAMMA 1000)
 
-(struct ivec
-  (iv   ;; (Vectorof Item) -- the items
-   Sw   ;; (Vectorof Real) -- sum of previous w (see p1157)
-   Sy   ;; (Vectorof Real) -- sum of previous y
-   Sz   ;; (Vectorof Real) -- sum of previous z
+(define p-tolerance 5)
+
+(define para%
+  (class object%
+    (init-field items     ;; (Vectorof Item)
+                targetw   ;; PositiveReal
+                [linepenalty 1]) ;; NNReal
+    (super-new)
+
+    ;; p1156: items must start with Box and end with (Penalty _ _ -inf.0)
+
+    ;; "w" = width
+    ;; "y" = stretchability
+    ;; "z" = shrinkability
+
+    (define len (vector-length items))      ;; "m" in BPL
+    (define Sw (cumsum Item-width items))   ;; (Vectorof Real), sum of previous w (p1157)
+    (define Sy (cumsum Item-stretch items)) ;; (Vectorof Real), sum of previous y
+    (define Sz (cumsum Item-shrink items))  ;; (Vectorof Real), sum of previous z
+
+    (define/public-final (get i) (vector-ref items i))
+    (define/public-final (get-w i) (Item-width (get i)))
+    (define/public-final (get-y i) (Item-stretch (get i)))
+    (define/public-final (get-z i) (Item-shrink (get i)))
+    (define/public-final (get-p i) (Item-penalty (get i)))
+    (define/public-final (get-f i) (and (>= i 0) (Item-flagged? (get i))))
+
+    (define/public-final (sum-w i j) (csumvec-diff Sw i j))
+    (define/public-final (sum-y i j) (csumvec-diff Sy i j))
+    (define/public-final (sum-z i j) (csumvec-diff Sz i j))
+
+    (define/public-final (legal-break? i) ;; p1125
+      ;; A linebreak is legal only at
+      ;; - a Glue item whose predecessor is a Box
+      ;; - a Penalty item with a penalty < +inf.0
+      (match (get i)
+        [(Penalty _ _ penalty _) (< penalty +inf.0)]
+        [(? Glue?) (and (> i 0) (Box? (get (sub1 i))))]
+        [_ #f]))
+
+    (define/public-final (forced-break? i)
+      (match (get i)
+        [(Penalty _ _ -inf.0 _) #t]
+        [_ #f]))
+
+    (define/public-final (after a)  ;; p1125, p1157
+      (let loop ([i (add1 a)])
+        (match (and (< i len) (get i))
+          [(or #f (? Box?) (Penalty _ _ -inf.0 _)) i]
+          [_ (loop (add1 i))])))
+
+    (define/public (line-actual-length a b) ;; p1126
+      (+ (sum-w a b)
+         (let ([itemb (get b)])
+           (if (Penalty? itemb) (Item-width itemb) 0))))
+
+    ;; line-adjustment-ratio : ... -> (U Real #f); p1126
+    (define/public (line-adjustment-ratio a b)
+      (define linew (line-actual-length a b))
+      (define stretch (sum-y a b))
+      (define shrink (sum-z a b))
+      (cond [(= linew targetw) 0]
+            [(< linew targetw)
+             (if (> stretch 0) (/ (- targetw linew) stretch) +inf.0)]
+            [(> linew targetw)
+             (if (> shrink 0) (/ (- targetw linew) shrink) -inf.0)]))
+
+    (define/public (line-badness a b) ;; p1127, \beta
+      (adjustment-ratio->badness (line-adjustment-ratio a b)))
+
+    (define/public (adjustment-ratio->badness r) ;; p1127, \beta
+      (cond [(< r -1) +inf.0]
+            [else (* 100 (expt (abs r) 3))]))
+
+    (define/public (line-demerits anode b r bfitness) ;; p1128, \delta
+      ;; See also "The errors of TEX", p631.
+      (define a (node-position anode))
+      (define aafter (node-after anode))
+      (define badness (adjustment-ratio->badness r))
+      (define penalty (get-p b))
+      (define alpha (if (and (get-f a) (get-f b)) ALPHA 0))
+      (define gamma (if (> (abs (- (node-fitness anode) bfitness)) 1) GAMMA 0))
+      (cond [(>= penalty 0)
+             (+ (sqr (+ linepenalty badness penalty)) alpha gamma)]
+            [(< -inf.0 penalty 0)
+             (+ (sqr (+ linepenalty badness)) (- (sqr penalty)) alpha gamma)]
+            [else ;; penalty = -inf.0
+             (+ (sqr (+ linepenalty badness)) alpha gamma)]))
+
+    (define/public (line-fitness-class a b) ;; p1128
+      (adjustment-ratio->class (line-adjustment-ratio a b)))
+
+    (define/public (adjustment-ratio->class r)
+      (cond [(< r -0.5) 0] ;; tight
+            [(< r 0.5) 1]  ;; normal
+            [(< r 1.0) 2]  ;; loose
+            [else 3]))     ;; very loose
+
+    ;; ------------------------------------------------------------
+    ;; Algorithm (p1148)
+
+    ;; Badness tolerance parameter p ...
+    ;; Looseness parameter q fixed at 0. (p1155)
+
+    ;; A feasible break (p1148) is a point b for which the whole paragraph
+    ;; can be broken into lines (at points [b1..bn=b] such that no line
+    ;; has a badness exceeding p.
+
+    (define/public (go)
+      (define result (go*))
+      (and result
+           (let loop ([n result] [acc null])
+             (define next (node-previous n))
+             (if next (loop next (cons (node-position n) acc)) acc))))
+
+    (define/public (go*)
+      (define anode0 (initial-node))
+      (define final-active
+        (for/fold ([active (list anode0)])
+                  ([b (in-range len)] #:when (legal-break? b))
+          (eprintf "b = ~s, active(~s) = ~e\n" b (length active) active)
+          (update-active-breakpoints active b)))
+      (and (pair? final-active) (argmin node-totdemerits final-active)))
+
+    (define/public (initial-node)
+      (node -1 0 0 1 0 #f))
+
+    (define/private (update-active-breakpoints active b)
+      ;; body of main loop
+      (define-values (passive feasible)
+        (for/fold ([passive null] [feasible null])
+                  ([anode (in-list active)])
+          (define r (line-adjustment-ratio (node-after anode) b))
+          (values (cond [(or (< r -1) (forced-break? b))
+                         (cons anode passive)]
+                        [else passive])
+                  (cond [(and (<= -1 r) (< r p-tolerance))
+                         (cons (make-break anode b r) feasible)]
+                        [else feasible]))))
+      (let ([active (if (pair? passive) (remq* passive active) active)])
+        (append (best-feasible-breaks feasible) active)))
+
+    (define/private (make-break anode b r)
+      (match-define (node a aafter aline afitness atotdemerits _) anode)
+      (define bfitness (adjustment-ratio->class r))
+      (define bdemerits (line-demerits anode b r bfitness))
+      (node b (after b) (add1 aline) bfitness (+ atotdemerits bdemerits) anode))
+
+    (define (best-feasible-breaks feasible)
+      ;; (p1159) Select at most one break per fitness, since we fix q=0.
+      (for/fold ([keep null])
+                ([fitness (in-range 0 4)])
+        (for/fold ([best #f]
+                   #:result (if best (cons best keep) keep))
+                  ([bnode (in-list feasible)]
+                   #:when (= fitness (node-fitness bnode)))
+          (if (or (not best) (< (node-totdemerits bnode) (node-totdemerits best))) bnode best))))
+
+    ))
+
+;; p1158
+(struct node
+  (position     ;; Nat/-1 -- index of breakpoint (-1 means start of paragraph)
+   after        ;; Nat -- after(position) = start of next line
+   line         ;; Nat -- number of line ending here
+   fitness      ;; fitness class of line ending here
+   totdemerits  ;; Real -- total demerits up to this breakpoint
+   previous     ;; Node/#f -- link to previous breakpoint
    ) #:transparent)
+
+;; FIXME: store adjustment-ratio in node!
+
+(define (node<? x y)
+  (let ([xline (node-line x)] [yline (node-line y)])
+    (or (< xline yline) (and (= xline yline) (< (node-fitness x) (node-fitness y))))))
 
 (define (cumsum f argv)
   (define len (vector-length argv))
@@ -46,87 +221,16 @@
 (define (csumvec-diff csvec i j)
   (- (vector-ref csvec j) (vector-ref csvec i)))
 
-(define (make-ivec items)
-  (define iv (list->vector items))
-  (define len (vector-length iv))
-  (define Sw (cumsum Item-width wv))
-  (define Sy (cumsum Item-stretch iv))
-  (define Sz (cumsum Item-shrink iv))
-  (ivec iv Sw Sy Sz))
-
-(define (ivec-ref ivec i) (vector-ref (ivec-iv ivec) i))
-(define (ivec-w ivec i) (Item-width (vector-ref (ivec-iv ivec) i)))
-(define (ivec-y ivec i) (Item-stretch (vector-ref (ivec-iv ivec) i)))
-(define (ivec-z ivec i) (Item-shrink (vector-ref (ivec-iv ivec) i)))
-(define (ivec-p ivec i) (Item-penalty (vector-ref (ivec-iv ivec) i)))
-(define (ivec-f ivec i) (Item-flagged? (vector-ref (ivec-iv ivec) i)))
-
-(define (ivec-sum-w ivec i j) (csumvec-diff (ivec-Sw ivec) i j))
-(define (ivec-sum-y ivec i j) (csumvec-diff (ivec-Sy ivec) i j))
-(define (ivec-sum-z ivec i j) (csumvec-diff (ivec-Sz ivec) i j))
-
-(define (ivec-legal-break? ivec i) ;; p1125
-  ;; A linebreak is legal only at
-  ;; - a Glue item whose predecessor is a Box
-  ;; - a Penalty item with a penalty < +inf.0
-  (match (ivec-ref ivec i)
-    [(Penalty _ _ penalty _) (< penalty +inf.0)]
-    [(? Glue?) (and (> i 0) (Box? (ivec-item ivec (sub1 i))))]
-    [_ #f]))
-
-(define (ivec-after ivec a)  ;; p1125, p1157
-  (define iv (ivec-iv ivec))
-  (define len (vector-length iv))
-  (let loop ([i (add1 a)])
-    (match (and (< i len) (vector-ref iv i))
-      [(or #f (? Box?) (Penalty _ _ -inf.0 _)) i]
-      [_ (loop (add1 i))])))
-
-(define (ivec-actual-length ivec a b) ;; p1126
-  (+ (ivec-sum-w ivec a b)
-     (let ([itemb (ivec-ref ivec b)])
-       (if (Penalty? itemb) (Penalty-width itemb) 0))))
-
-;; ivec-adjustment-ratio : ... -> (U Real #f); p1126
-(define (ivec-adjustment-ratio ivec a b targetw)
-  (define linew (ivec-actual-length ivec a b))
-  (define stretch (ivec-sum-y ivec a b))
-  (define shrink (ivec-sum-z ivec a b))
-  (cond [(= linew targetw) 0]
-        [(< linew targetw)
-         (if (> stretch 0) (/ (- targetw linew) stretch) #f)]
-        [(> linew targetw)
-         (if (> shrink 0) (/ (- targetw linew) shrink) #f)]))
-
-(define (ivec-badness ivec a b targetw) ;; p1127, \beta
-  (adjustment-ratio->badness (ivec-adjustment-ratio ivec a b targetw)))
-
-(define (adjustment-ratio->badness r) ;; p1127, \beta
-  (cond [(or (not r) (< r -1)) +inf.0]
-        [else (* 100 (expt (abs r) 3))]))
-
-(define (ivec-demerits ivec a b targetw) ;; p1128, \delta
-  ;; See also "The errors of TEX", p631.
-  (define r (ivec-adjustment-ratio ivec a b targetw))
-  (define badness (adjustment-ratio->badness r))
-  (define penalty (ivec-p ivec b))
-  (define LP 1) ;; linepenalty parameter
-  (define alpha 0) ;; FIXME! Extra penalty for consecutive hyphenated lines.
-  (cond [(>= penalty 0)
-         (+ (sqr (+ LP badness penalty)) alpha)]
-        [(< -inf.0 penalty 0)
-         (+ (sqr (+ LP badness)) (- (sqr penalty)) alpha)]
-        [else ;; penalty = -inf.0
-         (+ (sqr (+ LP badness)) alpha)]))
-
-;; Badness tolerance parameter p ...
-
-;; A feasible break (p1148) is a point b for which the whole paragraph
-;; can be broken into lines (at points [b1..bn=b] such that no line
-;; has a badness exceeding p.
-
-;; Looseness parameter q fixed at 0. (p1155)
-
 ;; ------------------------------------------------------------
-;; Algorithm (p1148)
 
+(define sp (Glue " " 6 3 2))
+(define words (for/list ([i (in-range 20)])
+                (define len (modulo (sqr i) 6))
+                (Box (make-string len #\x) (* len 6))))
+(define items (append (add-between words sp) (list (Penalty "!" 0 -inf.0 #f))))
+(define itemv (list->vector items))
+;;itemv
+
+(define para (new para% (items itemv) (targetw 50)))
+
+(send para go)
