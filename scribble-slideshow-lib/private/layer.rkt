e214ecd300289326731b6f9a9c5ba3b88ecaf65e
Ryan Culpepper <ryanc@racket-lang.org>
2021-11-18 22:08:03 +0100

    ... WIP

--- scribble-slideshow-lib/private/layer.rkt
+++ scribble-slideshow-lib/private/layer.rkt
@@ -4,27 +4,149 @@
 #lang racket/base
 (require racket/match
          racket/class
-         (only-in slideshow/base current-gap-size title-h get-client-h get-client-w)
+         (only-in slideshow/base
+                  current-gap-size margin title-h
+                  get-client-h get-client-w)
          pict
          ppict/pict
+         ppict/align
+         ppict/zone
          "style.rkt")
 (provide (all-defined-out))
+
+;; ============================================================
+;; Slide configs and zones
+
+(define slide-config<%>
+  (interface ()
+    ;; Slide-specific
+    slide-title? ;; -> Boolean
+    slide-layout ;; -> Layout
+    slide-aspect ;; -> Aspect
+    ;; Settings
+    clientw ;; Aspect -> Real
+    clienth ;; Aspect -> Real
+    titleh  ;; -> Real
+    margin  ;; -> Real
+    gap     ;; -> Real
+    ))
+
+;; FIXME: Move to slide.rkt
+(define slide-config%
+  (class object%
+    (init-field title? layout aspect)
+    (super-new)
+    ;; Slide-specific
+    (define/public (slide-title?) title?)
+    (define/public (slide-layout) layout)
+    (define/public (slide-aspect) aspect)
+    ;; Global settings
+    (define/public (clientw aspect) (get-client-w #:aspect aspect))
+    (define/public (clienth aspect) (get-client-h #:aspect aspect))
+    (define/public (titleh) title-h)
+    (define/public (get-margin) margin)
+    (define/public (gap [n 1]) (* n (current-gap-size)))
+
+    (define/public (screenw aspect)
+      (+ (clientw aspect) margin margin))
+    (define/public (screenh aspect)
+      (+ (clienth aspect) margin margin))
+    (define/public (get-screen-dx aspect)
+      (if aspect (/ (- (clientw #f) (clientw aspect)) 2) 0))
+
+    (define/public (slide-zone-f name aspect)
+      (case name
+        ;; Vertically centered, title?-independent
+        [(main)
+         (define dh (+ title-h (gap 2)))
+         (values (clientw aspect) (- (clienth aspect) dh dh) (get-screen-dx aspect) dh)]
+        [(tall-main)
+         (define dh (+ title-h (gap 1)))
+         (values (clientw aspect) (- (clienth aspect) dh dh) (get-screen-dx aspect) dh)]
+        [(full)
+         (values (clientw aspect) (clienth aspect) (get-screen-dx aspect) 0)]
+        [(screen)
+         (values (screenw aspect) (screenh aspect) (- (get-screen-dx aspect) margin) (- margin))]
+        ;; Vertically centered, title?-dependent
+        [(main/full)
+         (if (slide-title?)
+             (slide-zone-f 'main aspect)
+             (slide-zone-f 'full aspect))]
+        ;; Non-centered, title?-independent
+        [(body)
+         (define dh (+ title-h (* 2 (current-gap-size))))
+         (values (clientw aspect) (- (clienth aspect) dh) (get-screen-dx aspect) dh)]
+        [(tall-body)
+         (define dh (+ title-h (* 1 (current-gap-size))))
+         (values (clientw aspect) (- (clienth aspect) dh) (get-screen-dx aspect) dh)]
+        ;; [(body/client) _]
+        ;; [(tall-body/client) _]
+        [else (error 'slide-zone "unknown slide-zone name: ~e" name)]))
+    ))
+
+(define current-slide-config (make-parameter #f))
+(define (get-slide-config who)
+  (or (current-slide-config)
+      (error who "no slide configuration available")))
+
+;; slide-zone : Symbol #:aspect Aspect -> Zone
+;; The result assumes that the initial scene is (get-full-page #:aspect #f)
+;; --- that is, with dimensions (get-client-{w,h} #:aspect #f) --- and will
+;; be displayed centered on the screen.
+(define (slide-zone name #:aspect [aspect #f])
+  (define (zone-f . args)
+    (send (get-slide-config 'slide-zone) slide-zone-f name aspect))
+  (hash-ref! slide-zone-table (cons name aspect)
+             (lambda () (make-zone zone-f))))
+
+(define slide-zone-table (make-hash))
+
+#|
+;; For default layer:
+
+(local-require (only-in ppict/private/ppict placer-base% apply-compose)) ;; FIXME!
+(define overflow-placer%
+  (class placer-base%
+    (init-field halign valign overflow-valign compose sep)
+    (super-new)
+    (define/override (place* scene iw ih ix iy elems)
+      (define x (+ ix (* iw (align->frac halign))))
+      (define-values (newpict newsep) (apply-compose compose sep elems))
+      (cond [(<= (pict-height newpict) ih)
+             (define y (+ iy (* ih (align->frac valign))))
+             (pin-over/align scene x y halign valign newpict)]
+            [else
+             (define y (+ iy (* ih (align->frac valign))))
+             (pin-over/align scene x y halign overflow-valign newpict)]))
+    ))
+
+(define (overflow-placer #:halign [halign 'center]
+                         #:valign [valign 'center]
+                         #:overflow-valign [overflow-valign 'top]
+                         #:compose [compose (halign->vcompose halign)]
+                         #:sep [sep 0])
+  (new overflow-placer% (halign halign) (valign valign) (overflow-valign overflow-valign)
+       (compose compose) (sep sep)))
+|#
 
 ;; ============================================================
 ;; Layers
 
 (define layer<%>
   (interface ()
-    get-z
+    get-z        ;; -> ExtendedReal
     update-style ;; StyleHash -> StyleHash
 
     ;; type LayerPre
+
+    ;; Used to determine placement of current content based on this layer's
+    ;; content on all related slides (eg next, alts).
+
     update-pre   ;; LayerPre/#f Pict -> LayerPre
     max-pre      ;; LayerPre LayerPre -> LayerPre
 
-    place        ;; Boolean Layout (Listof Pict) LayerPre Pict -> Pict
-    ;; Places the contents onto the given base, where base is a full-page pict
-    ;; for the given aspect.
+    place        ;; (Listof Pict) LayerPre Pict -> Pict
+    ;; Places the contents onto the given base, where base is a full-page pict.
     ))
 
 (define (layer? v) (is-a? v layer<%>))
@@ -34,6 +156,8 @@
     (init-field [style (hasheq)]    ;; StyleHash, should set 'block-width
                 [z (next-auto-z)])  ;; Real
     (super-new)
+
+    ;; FIXME: change style field to upstyle (SPStyle -> SPStyle) ??
 
     (define/public (get-z) z)
 
@@ -54,9 +178,13 @@
     (init-field [gap (current-gap-size)])
     (super-new)
 
+    ;; type LayerPre = Real  -- height of all picts so far
+
+    ;; Placement of current content is determined by maximum height of content
+    ;; in this layer on all related slides. For example: coord, grid, etc.
+
     (define/public (get-gap) gap)
 
-    ;; type LayerPre = Real  -- height of all picts so far
     (define/override (update-pre lpre p)
       (if lpre
           (+ lpre (get-gap) (pict-height p))
@@ -70,6 +198,11 @@
     (super-new)
 
     ;; type LayerPre = (Listof BlankPict)
+
+    ;; Placement of current content is determined by maximum number of picts
+    ;; (and maximum dimensions per pict index) in this layer on all related
+    ;; slides. For example: cascade, tile.
+
     (define/override (update-pre lpre p)
       (append (or lpre null) (list (blank (pict-width p) (pict-height p)))))
     (define/override (max-pre ps1 ps2)
@@ -81,6 +214,43 @@
                         (max (pict-height p1) (pict-height p2)))
                (max-pre ps1 ps2))]))
     ))
+
+(define layer%
+  (class h-layer-base%
+    (init-field placer  ;; RefpointPlacer
+                zone)   ;; Zone, used to update style with width?
+    (inherit get-gap)
+    (super-new)
+
+    (define zplacer (subplacer placer zone))
+    (define halign
+      (or (send zplacer check-associative-vcompose)
+          (error 'layer "placer has incompatible compose function: ~e" placer)))
+
+    ;; place : (Listof Pict) LayerPre Pict -> Pict
+    (define/override (place ps lpre base)
+      (define p (combine-picts ps lpre))
+      (send zplacer place base (list p)))
+
+    ;; combine-picts : (Listof Pict) LayerPre -> Pict
+    (define/public (combine-picts ps lpre)
+      (define p (send zplacer compose-elements ps))
+      (inset-to/align p #f lpre (make-align halign 'c)))
+    ))
+
+;; FIXME! To avoid a dependency from layer.rkt to slideshow/base, the layer
+;; place method should take a slide-config argument and use that to get
+;; client/screen dimensions.
+
+;; Downside: we can't ask a layer for its width (or height) without a
+;; slideinfo. (Only if using aspect = #f...) This is okay (maybe?), because we
+;; don't *need* the width to update the width until we do the scribble->picts
+;; conversion, but it means that all width-dependent things must be delayed!
+
+;; Maybe add an indirection so that named layers (& zones & placers ?) can be
+;; registered entirely separately from the document?
+
+;; XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 (define default-layer%
   (class h-layer-base%
@@ -174,47 +344,6 @@
 
 (define default-layer (new default-layer%))
 
-(define layer%
-  (class h-layer-base%
-    (init-field placer
-                [aspect 'fullscreen];; Determines client area
-                [layout 'tall])     ;; Determines client area
-    (inherit get-gap)
-    (super-new)
-
-    (define/public (get-placer) placer)
-
-    ;; place : Boolean Layout (Listof Pict) LayerPre Pict -> Pict
-    ;; Result has same bounding box as base, places contents correctly
-    ;; if result is centered on slide.
-    (define/override (place title? slide-layout ps lpre base)
-      (define elayout (get-effective-layout* title? slide-layout lpre))
-      (define body (combine-picts ps lpre))
-      (define client (ppict-add (get-client-ppict title? elayout) body))
-      (pin-over base
-                (/ (- (pict-width base) (pict-width client)) 2)
-                (+ (/ (- (pict-height base) (pict-height client)) 2)
-                   (get-client-y title? elayout 0 0))
-                client))
-
-    ;; get-effective-layout* : Layout Real -> Layout
-    (define/public (get-effective-layout* title? slide-layout body-h)
-      (let ([layout* (case layout [(slide-layout) slide-layout] [else layout])])
-        (get-effective-layout title? aspect layout* body-h)))
-
-    ;; get-client-ppict : Boolean ELayout -> PPict
-    (define/public (get-client-ppict title? elayout)
-      (ppict-do (blank (get-client-w #:aspect aspect)
-                       (- (get-client-h #:aspect aspect)
-                          (get-client-y title? elayout 0 0)))
-                #:go (get-placer)))
-
-    ;; combine-picts : (Listof Pict) LayerPre -> Pict
-    (define/public (combine-picts ps lpre)
-      (define p (apply vc-append (get-gap) ps))
-      (inset p 0 (- lpre (pict-height p)) 0 0))
-    ))
-
 (define (layer<? a b)
   (< (send a get-z) (send b get-z)))
 
@@ -236,3 +365,48 @@
        (placer (coord rx1 ry align #:sep gap))
        (style (hash-set style 'block-width w))
        (gap gap) (aspect aspect) (layout layout) (z z)))
+
+
+;; ============================================================
+
+(module+ main
+  (require ppict/slideshow2
+           (only-in slideshow/base slide get-full-page t))
+
+  (define (test-slide zname title)
+    (parameterize ((current-slide-config
+                    (new slide-config% (title? (and title #t)) (layout #f) (aspect #f))))
+      (slide #:title title #:layout 'tall
+             (inset
+              (ppict-do (frame (get-full-page #:aspect #f))
+                        #:go (subplacer (coord 0 0 'cc) (slide-zone zname))
+                        (colorize (disk 20) "red")
+                        #:go (subplacer (coord 1 1 'cc) (slide-zone zname))
+                        (colorize (disk 20) "blue")
+                        #:go (subplacer (coord 1/2 1/2 'cc) (slide-zone zname))
+                        (t (format "zone: ~e" zname)))
+              0
+              (if title (- (+ title-h (* 1 (current-gap-size)))) 0)))))
+
+  (test-slide 'main "main")
+  (test-slide 'main #f)
+
+  (test-slide 'tall-main "tall-main")
+  (test-slide 'tall-main #f)
+
+  (test-slide 'full "full")
+  (test-slide 'full #f)
+
+  (test-slide 'screen "screen")
+  (test-slide 'screen #f)
+
+  (test-slide 'main/full "main/full")
+  (test-slide 'main/full #f)
+
+  (test-slide 'body "body")
+  (test-slide 'body #f)
+
+  (test-slide 'tall-body "tall-body")
+  (test-slide 'tall-body #f)
+
+  )
