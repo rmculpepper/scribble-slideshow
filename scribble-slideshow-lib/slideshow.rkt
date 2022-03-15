7ebd9c18c9e29e0fdcd70f07a44b88f5f2ea6d62
Ryan Culpepper <ryanc@racket-lang.org>
2022-03-15 12:54:38 +0100

    ... add slideshow module

---
+++ scribble-slideshow-lib/slideshow.rkt
@@ -0,0 +1,7 @@
+;; Copyright 2022 Ryan Culpepper.
+;; Licensed under the Apache 2.0 license. See LICENSE.
+
+#lang racket/base
+(require (submod "private/slide.rkt" slideshow))
+(provide scribble-slides
+         scribble-slides*)
